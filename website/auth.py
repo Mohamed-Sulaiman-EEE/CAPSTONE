63d4c5c9a7fc3c853d1a12ea448dca804c70cfc9
Mohamed K Sulaiman <sulaimanhamsa555@gmail.com>
2022-10-19 14:15:18 +0530

    Mehh

--- website/auth.py
+++ website/auth.py
@@ -8,7 +8,6 @@ from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
 
-
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -20,7 +19,7 @@ def login():
             if check_password_hash(user.password, password):
                 flash('Logged in successfully!', category='success')
                 login_user(user, remember=True)
-                return redirect(url_for('views.home'))
+                return redirect(url_for('views.user_home'))
             else:
                 flash('Incorrect password, try again.', category='error')
         else:
@@ -33,7 +32,7 @@ def login():
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('auth.login'))
+    return redirect(url_for('views.home'))
 
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
@@ -62,6 +61,52 @@ def sign_up():
             db.session.commit()
             login_user(new_user, remember=True)
             flash('Account created!', category='success')
-            return redirect(url_for('views.home'))
+            return redirect(url_for('views.user_home'))
 
     return render_template("sign_up.html", user=current_user)
+
+
+
+@auth.route('/conductor-login', methods=['GET', 'POST'])
+def conductor_login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.conductor_home'))
+            else:
+                flash('Incorrect password, try again.', category='error')
+        else:
+            flash('Email does not exist.', category='error')
+    
+    return render_template("conductor_login.html", user = current_user)
+
+
+
+
+
+
+    
+@auth.route('/admin-login', methods=['GET', 'POST'])
+def admin_login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.admin_home'))
+            else:
+                flash('Incorrect password, try again.', category='error')
+        else:
+            flash('Email does not exist.', category='error')
+    
+    return render_template("admin_login.html", user = current_user)
--- website/views.py
+++ website/views.py
@@ -1,30 +1,57 @@
+from unicodedata import name
 from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_required, current_user
-from .models import Note
+from .models import Note, User
 from . import db
 import json
 # END OF IMPORTS
-
 views = Blueprint('views', __name__)
-
 #....................................................................................
 @views.route('/', methods=['GET', 'POST'])
-
 def home():
-    
     return render_template("home.html", user=current_user)
 
-
 #....................................................................................
 
-#....................................................................................
+#.........................USER FUNCTIONS...........................................................
 @views.route('/user-home', methods=['GET', 'POST'])
 @login_required
-def UserHome():
-    return render_template("user_home.html" , user = current_user)
+def user_home():
+    name = current_user.first_name
+    return render_template("user_home.html" , user = current_user, name = name)
 
+
+@views.route('/user-enquire-route', methods=['GET', 'POST'])
+@login_required
+def user_enquire_route():
+    
+    return render_template("user_enquire_route.html" , user = current_user )
+
+
+
+@views.route('/user-travel-history', methods=['GET', 'POST'])
+@login_required
+def user_travel_history():
+    return render_template("user_travel_history.html" , user = current_user)
+
+
+
+@views.route('/user-wallet', methods=['GET', 'POST'])
+@login_required
+def user_wallet():
+    return render_template("user_wallet.html" , user = current_user)
+
+#.................................CONDUCTOR FUNCTIONS .............................................
+
+@views.route('/conductor-home', methods=['GET', 'POST'])
+@login_required
+def conductor_home():
+    return render_template("conductor_home.html" , user = current_user)
 
 #....................................................................................
-#....................................................................................
 
+@views.route('/admin-home', methods=['GET', 'POST'])
+@login_required
+def admin_home():
+    return render_template("admin_home.html" , user = current_user)
 #....................................................................................
